00e8eaf2834c09124ec87a5af67594e70b000afd
Capt-Reaps <ivan.mejorada05@gmail.com>
2026-02-06 02:20:26 +0800

    trail for the nav bar

---
+++ app/src/main/java/com/example/LuxxHavenApp/BookingsActivity.java
@@ -0,0 +1,42 @@
+package com.example.LuxxHavenApp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BookingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookings); // Make sure you create this XML file!
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_bookings);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_home) {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                } else if (itemId == R.id.nav_wishlist) {
+                    startActivity(new Intent(getApplicationContext(), WishlistActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                } else if (itemId == R.id.nav_bookings) {
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/LuxxHavenApp/WishlistActivity.java
@@ -0,0 +1,43 @@
+package com.example.LuxxHavenApp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class WishlistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_wishlist);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_home) {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                } else if (itemId == R.id.nav_wishlist) {
+                    return true; // Already here
+                } else if (itemId == R.id.nav_bookings) {
+                    startActivity(new Intent(getApplicationContext(), BookingsActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
